64b691d35f90b6a8026982bbd199cb2bc69dc34d
Telegram & App source Code <khithlainhtet2021@gmail.com>
2026-03-05 20:38:19 +0630

    KHITMUSIC

---
+++ khitmusic/utils/decorators/__init__.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------
+# 🔸 khitmusic Project
+# 🔹 Developed & Maintained by: Stark (https://github.com/khithlainhtet)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by khithlainhtet# -----------------------------------------------
+from .admins import *
+from .language import *
---
+++ khitmusic/utils/inline/queue.py
@@ -0,0 +1,106 @@
+# -----------------------------------------------
+# 🔸 khitmusic Project
+# 🔹 Developed & Maintained by: Stark (https://github.com/khithlainhtet)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by khithlainhtet# -----------------------------------------------
+from typing import Union
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from khitmusic import app
+
+def queue_markup(
+    _,
+    DURATION,
+    CPLAY,
+    videoid,
+    played: Union[bool, int] = None,
+    dur: Union[bool, int] = None,
+):
+    not_dur = [
+        [
+            InlineKeyboardButton(
+                text=_["QU_B_1"],
+                callback_data=f"GetQueued {CPLAY}|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+            ),
+        ]
+    ]
+    dur = [
+        [
+            InlineKeyboardButton(
+                text=_["QU_B_2"].format(played, dur),
+                callback_data="GetTimer",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["QU_B_1"],
+                callback_data=f"GetQueued {CPLAY}|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+            ),
+        ],
+    ]
+    upl = InlineKeyboardMarkup(not_dur if DURATION == "Unknown" else dur)
+    return upl
+
+
+def queue_back_markup(_, CPLAY):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"queue_back_timer {CPLAY}",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ]
+        ]
+    )
+    return upl
+
+
+def aq_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+        ],
+        [
+            InlineKeyboardButton(text=" ▷ ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text=" II ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text=" ‣‣I ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text=" ▢ ", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
+        ],
+    ]
+    return buttons
+
+
+def replay_markup(_, videoid):
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="⌯ ʀᴇᴘʟᴀʏ ⌯",
+                    callback_data=f"replay_song {videoid}",
+                ),
+                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            ]
+        ]
+    )
